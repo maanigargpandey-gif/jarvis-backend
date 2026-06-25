5cd032e57c0c829efbb586edfa82c81f603e5c33
maanigargpandey-gif <maanigargpandey@gmail.com>
2026-06-25 18:36:07 +0530

    Update auth.py

--- auth.py
+++ auth.py
@@ -1,4 +1,3 @@
-# auth.py
 from fastapi import HTTPException, Security
 from fastapi.security import APIKeyHeader
 from modules.identity_core import identity
@@ -8,7 +7,7 @@ api_key_header = APIKeyHeader(name="X-Creator-Token", auto_error=False)
 
 def verify_creator(creator_token: str = Security(api_key_header)):
     if not creator_token:
-        print("⚠️ Authentication Failed: No Token Provided")
+        print("⚠️ Auth Failed: No Token Provided")
         raise HTTPException(status_code=401, detail="Missing Creator Token")
         
     if not identity.verify_access(creator_token):
