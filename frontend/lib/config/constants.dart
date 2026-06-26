d40eb8b143b0df65369b8911d08aa5fc82a3d27e
maanigargpandey-gif <maanigargpandey@gmail.com>
2026-06-27 03:40:11 +0530

    Create constants.dart

---
+++ frontend/lib/config/constants.dart
@@ -0,0 +1,10 @@
+class AppConstants {
+  static const String baseUrl = 'http://localhost:7860';
+  static const String apiVersion = '/api/v1';
+  static const String adminToken = 'mani_jarvis_admin_786';
+  static const String creatorName = 'Mani Pandey';
+  static const String creatorEmail = 'maanigargpandey@gmail.com';
+  
+  static const Duration mfaTimeout = Duration(minutes: 5);
+  static const int maxLoginAttempts = 3;
+}
