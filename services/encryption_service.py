e9779131581f39e7b53b301f7e5343dc6748a830
maanigargpandey-gif <maanigargpandey@gmail.com>
2026-06-27 03:26:58 +0530

    Create encryption_service.py

---
+++ services/encryption_service.py
@@ -0,0 +1,24 @@
+from cryptography.fernet import Fernet
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
+import base64
+from core.config import settings
+
+class EncryptionService:
+    def __init__(self):
+        salt = b'zarvish_encryption_salt'
+        kdf = PBKDF2HMAC(
+            algorithm=hashes.SHA256(),
+            length=32,
+            salt=salt,
+            iterations=100000,
+        )
+        key = base64.urlsafe_b64encode(kdf.derive(settings.ENCRYPTION_KEY))
+        self.cipher = Fernet(key)
+    
+    def encrypt(self, data: str) -> str:
+        return self.cipher.encrypt(data.encode()).decode()
+    
+    def decrypt(self, encrypted_data: str) -> str:
+        return self.cipher.decrypt(encrypted_data.encode()).decode()
+      
